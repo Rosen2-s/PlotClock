3cb67c1ec3cc96f34fdc430aad7da1fc16cfe76f
Rosen2-s <juanimof_5@hotmail.com>
2018-07-17 01:12:31 -0300

    Add files via upload

---
+++ plotclock.ino
@@ -0,0 +1,448 @@
+
+#define SERVOFAKTORLEFT 610
+#define SERVOFAKTORRIGHT 610
+
+// posición cero del servo derecho e izquierdo
+
+//#define SERVOLEFTNULL 2250
+//#define SERVORIGHTNULL 920
+#define SERVOLEFTNULL 1650
+#define SERVORIGHTNULL 1650
+//#define SERVORIGHTNULL 900
+
+#define SERVOPINLIFT  2
+#define SERVOPINLEFT  3
+#define SERVOPINRIGHT 4
+
+// posiciones del servo de levantamiento 
+
+#define LIFT0 1120  // en superficie de dibujo
+#define LIFT1 825  // entre números
+#define LIFT2 605  // yendo hacia el limpiador 
+
+// velocidad del brazo que levanta, Nº más alto menos velocidad
+//#define LIFTSPEED 1500
+#define LIFTSPEED 3000
+
+// longitud de brazos.
+#define L1 35
+#define L2 57.1
+#define L3 13.2
+
+// puntos de origen de los servos izquierdo y derecho
+#define O1X 22
+#define O1Y -25
+#define O2X 47
+#define O2Y -25
+//#define O1X 21
+//#define O1Y -25
+//#define O2X 105
+//#define O2Y -25
+
+#define PARKX 77
+#define PARKY 47
+#define ERASEMAXX 60
+
+#include <Time.h> 
+#include <Servo.h>
+
+#ifdef REALTIMECLOCK
+
+
+  #include <Wire.h>
+  #include <DS1307RTC.h>    
+#endif
+
+int servoLift = 1500;
+
+Servo servo1;  // 
+Servo servo2;  // 
+Servo servo3;  // 
+
+volatile double lastX = 75;
+volatile double lastY = PARKY;
+
+int last_min = 0;
+
+void setup() 
+{ 
+#ifdef REALTIMECLOCK
+  Serial.begin(9600);
+  //while (!Serial) { ; } 
+
+  // setear la hora correspondiente sólo poner hh mm
+  tmElements_t tm;
+  if (RTC.read(tm)) 
+  {
+    setTime(tm.Hour,tm.Minute,tm.Second,tm.Day,tm.Month,tm.Year);
+    Serial.println("DS1307 time is set OK.");
+  } 
+  else 
+  {
+    if (RTC.chipPresent())
+    {
+      Serial.println("DS1307 is stopped.  Please run the SetTime example to initialize the time and begin running.");
+    } 
+    else 
+    {
+      Serial.println("DS1307 read error!  Please check the circuitry.");
+    } 
+    
+    setTime(15,05,0,0,0,0);
+  }
+#else  
+  
+#endif
+
+  drawTo(PARKX, PARKY);
+  lift(0);
+  servo1.attach(SERVOPINLIFT);  //  servo de levantamiento
+  servo2.attach(SERVOPINLEFT);  //  servo izquierdo
+  servo3.attach(SERVOPINRIGHT);  //  servo derecho
+  delay(1000);
+
+} 
+
+void loop() 
+{ 
+
+#ifdef CALIBRATION
+
+  // los servos tienen 90º entre movimientos, paralelos al eje x e y 
+  drawTo(-3, 29.2);
+  delay(500);
+  drawTo(74.1, 28);
+  delay(500);
+
+#else 
+
+
+  int i = 0;
+  if (last_min != minute()) {
+
+    if (!servo1.attached()) servo1.attach(SERVOPINLIFT);
+    if (!servo2.attached()) servo2.attach(SERVOPINLEFT);
+    if (!servo3.attached()) servo3.attach(SERVOPINRIGHT);
+
+    lift(0);
+
+    hour();
+    while ((i+1)*10 <= hour())
+    {
+      i++;
+    }
+    // borrar 2x
+    number(3, 3, 111, 1);
+    number(3, 3, 111, 1);
+    // hora1
+    number(5, 25, i, 0.9);
+    // hora2
+    number(19, 25, (hour()-i*10), 0.9);
+    
+    number(28, 25, 11, 0.9);
+
+    i=0;
+    while ((i+1)*10 <= minute())
+    {
+      i++;
+    }
+    // minuto1
+    number(34, 25, i, 0.9);
+    // minuto2
+    number(48, 25, (minute()-i*10), 0.9);
+    lift(2);
+    drawTo(PARKX, PARKY);
+    lift(1);
+    last_min = minute();
+
+    servo1.detach();
+    servo2.detach();
+    servo3.detach();
+  }
+
+#endif
+
+} 
+
+// escribir el número con bx siendo el punto de origen de más abajo a la derecha la escala 1 es igual a una letra de 20mm de alto 
+// la estructura sigue el principio de moverse primero al punto de comienzo de numeral, bajar el brazo, dibujar el número y levantar
+void number(float bx, float by, int num, float scale) {
+
+  switch (num) {
+
+  case 0:
+    drawTo(bx + 12 * scale, by + 6 * scale);
+    lift(0);
+    bogenGZS(bx + 7 * scale, by + 10 * scale, 10 * scale, -0.8, 6.7, 0.5);
+    lift(1);
+    break;
+  case 1:
+
+    drawTo(bx + 3 * scale, by + 15 * scale);
+    lift(0);
+    drawTo(bx + 10 * scale, by + 20 * scale);
+    drawTo(bx + 10 * scale, by + 0 * scale);
+    lift(1);
+    break;
+  case 2:
+    drawTo(bx + 2 * scale, by + 12 * scale);
+    lift(0);
+    bogenUZS(bx + 8 * scale, by + 14 * scale, 6 * scale, 3, -0.8, 1);
+    drawTo(bx + 1 * scale, by + 0 * scale);
+    drawTo(bx + 12 * scale, by + 0 * scale);
+    lift(1);
+    break;
+  case 3:
+    drawTo(bx + 2 * scale, by + 17 * scale);
+    lift(0);
+    bogenUZS(bx + 5 * scale, by + 15 * scale, 5 * scale, 3, -2, 1);
+    bogenUZS(bx + 5 * scale, by + 5 * scale, 5 * scale, 1.57, -3, 1);
+    lift(1);
+    break;
+  case 4:
+    drawTo(bx + 10 * scale, by + 0 * scale);
+    lift(0);
+    drawTo(bx + 10 * scale, by + 20 * scale);
+    drawTo(bx + 2 * scale, by + 6 * scale);
+    drawTo(bx + 12 * scale, by + 6 * scale);
+    lift(1);
+    break;
+  case 5:
+    drawTo(bx + 2 * scale, by + 5 * scale);
+    lift(0);
+    bogenGZS(bx + 5 * scale, by + 6 * scale, 6 * scale, -2.5, 2, 1);
+    drawTo(bx + 5 * scale, by + 20 * scale);
+    drawTo(bx + 12 * scale, by + 20 * scale);
+    lift(1);
+    break;
+  case 6:
+    drawTo(bx + 2 * scale, by + 10 * scale);
+    lift(0);
+    bogenUZS(bx + 7 * scale, by + 6 * scale, 6 * scale, 2, -4.4, 1);
+    drawTo(bx + 11 * scale, by + 20 * scale);
+    lift(1);
+    break;
+  case 7:
+    drawTo(bx + 2 * scale, by + 20 * scale);
+    lift(0);
+    drawTo(bx + 12 * scale, by + 20 * scale);
+    drawTo(bx + 2 * scale, by + 0);
+    lift(1);
+    break;
+  case 8:
+    drawTo(bx + 5 * scale, by + 10 * scale);
+    lift(0);
+    bogenUZS(bx + 5 * scale, by + 15 * scale, 5 * scale, 4.7, -1.6, 1);
+    bogenGZS(bx + 5 * scale, by + 5 * scale, 5 * scale, -4.7, 2, 1);
+    lift(1);
+    break;
+
+  case 9:
+    drawTo(bx + 9 * scale, by + 11 * scale);
+    lift(0);
+    bogenUZS(bx + 7 * scale, by + 15 * scale, 5 * scale, 4, -0.5, 1);
+    drawTo(bx + 5 * scale, by + 0);
+    lift(1);
+    break;
+
+  case 111:
+
+    lift(0);
+    drawTo(70, 46);
+    drawTo(ERASEMAXX, 43);
+
+    drawTo(ERASEMAXX, 49);
+    drawTo(5, 49);
+    drawTo(5, 45);
+    drawTo(ERASEMAXX, 45);
+    drawTo(ERASEMAXX, 40);
+
+    drawTo(5, 40);
+    drawTo(5, 35);
+    drawTo(ERASEMAXX, 35);
+    drawTo(ERASEMAXX, 30);
+
+    drawTo(5, 30);
+    drawTo(5, 25);
+    drawTo(ERASEMAXX, 25);
+    drawTo(ERASEMAXX, 20);
+
+    drawTo(5, 20);
+    drawTo(60, 44);
+
+    drawTo(PARKX, PARKY);
+    lift(2);
+
+    break;
+
+  case 11:
+    drawTo(bx + 5 * scale, by + 15 * scale);
+    lift(0);
+    bogenGZS(bx + 5 * scale, by + 15 * scale, 0.1 * scale, 1, -1, 1);
+    lift(1);
+    drawTo(bx + 5 * scale, by + 5 * scale);
+    lift(0);
+    bogenGZS(bx + 5 * scale, by + 5 * scale, 0.1 * scale, 1, -1, 1);
+    lift(1);
+    break;
+
+  }
+}
+
+
+
+void lift(char lift) {
+  switch (lift) {
+    
+
+  case 0: //850
+
+      if (servoLift >= LIFT0) {
+      while (servoLift >= LIFT0) 
+      {
+        servoLift--;
+        servo1.writeMicroseconds(servoLift);        
+        delayMicroseconds(LIFTSPEED);
+      }
+    } 
+    else {
+      while (servoLift <= LIFT0) {
+        servoLift++;
+        servo1.writeMicroseconds(servoLift);
+        delayMicroseconds(LIFTSPEED);
+
+      }
+
+    }
+
+    break;
+
+  case 1: //150
+
+    if (servoLift >= LIFT1) {
+      while (servoLift >= LIFT1) {
+        servoLift--;
+        servo1.writeMicroseconds(servoLift);
+        delayMicroseconds(LIFTSPEED);
+
+      }
+    } 
+    else {
+      while (servoLift <= LIFT1) {
+        servoLift++;
+        servo1.writeMicroseconds(servoLift);
+        delayMicroseconds(LIFTSPEED);
+      }
+
+    }
+
+    break;
+
+  case 2:
+
+    if (servoLift >= LIFT2) {
+      while (servoLift >= LIFT2) {
+        servoLift--;
+        servo1.writeMicroseconds(servoLift);
+        delayMicroseconds(LIFTSPEED);
+      }
+    } 
+    else {
+      while (servoLift <= LIFT2) {
+        servoLift++;
+        servo1.writeMicroseconds(servoLift);        
+        delayMicroseconds(LIFTSPEED);
+      }
+    }
+    break;
+  }
+}
+
+
+void bogenUZS(float bx, float by, float radius, int start, int ende, float sqee) {
+  float inkr = -0.05;
+  float count = 0;
+
+  do {
+    drawTo(sqee * radius * cos(start + count) + bx,
+    radius * sin(start + count) + by);
+    count += inkr;
+  } 
+  while ((start + count) > ende);
+
+}
+
+void bogenGZS(float bx, float by, float radius, int start, int ende, float sqee) {
+  float inkr = 0.05;
+  float count = 0;
+
+  do {
+    drawTo(sqee * radius * cos(start + count) + bx,
+    radius * sin(start + count) + by);
+    count += inkr;
+  } 
+  while ((start + count) <= ende);
+}
+
+
+void drawTo(double pX, double pY) {
+  double dx, dy, c;
+  int i;
+
+  // dx dy del nuevo punto 
+  dx = pX - lastX;
+  dy = pY - lastY;
+  // longitud del camino en mm
+  c = floor(4 * sqrt(dx * dx + dy * dy));
+
+  if (c < 1) c = 1;
+
+  for (i = 0; i <= c; i++) {
+    // dibuja la linea punto apunto 
+    set_XY(lastX + (i * dx / c), lastY + (i * dy / c));
+
+  }
+
+  lastX = pX;
+  lastY = pY;
+}
+
+double return_angle(double a, double b, double c) {
+ 
+  return acos((a * a + c * c - b * b) / (2 * a * c));
+}
+
+void set_XY(double Tx, double Ty) 
+{
+  delay(1);
+  double dx, dy, c, a1, a2, Hx, Hy;
+
+  // calcula triangulo entre fibron, servoLeft y la unión del brazo
+  // cartesiano dx/dy
+  dx = Tx - O1X;
+  dy = Ty - O1Y;
+
+ 
+  c = sqrt(dx * dx + dy * dy); // 
+  a1 = atan2(dy, dx); //
+  a2 = return_angle(L1, L2, c);
+
+  servo2.writeMicroseconds(floor(((a2 + a1 - M_PI) * SERVOFAKTORLEFT) + SERVOLEFTNULL));
+
+
+  a2 = return_angle(L2, L1, c);
+  Hx = Tx + L3 * cos((a1 - a2 + 0.621) + M_PI); //36,5°
+  Hy = Ty + L3 * sin((a1 - a2 + 0.621) + M_PI);
+
+ 
+  dx = Hx - O2X;
+  dy = Hy - O2Y;
+
+  c = sqrt(dx * dx + dy * dy);
+  a1 = atan2(dy, dx);
+  a2 = return_angle(L1, (L2 - L3), c);
+
+  servo3.writeMicroseconds(floor(((a1 - a2) * SERVOFAKTORRIGHT) + SERVORIGHTNULL));
+
+}
+
